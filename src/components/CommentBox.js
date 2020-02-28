10a6a3c53a72f22a24bc68d45fb1da38ac62b1e3
Haryanto <haryantowang09@gmail.com>
2020-02-28 13:51:22 +0700

    Setup Redux, TestCase

---
+++ src/components/CommentBox.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from 'actions';
+
+class CommentBox extends Component {
+  state = { comment: '' };
+
+  handleChange = event => {
+    this.setState({ comment: event.target.value });
+    //RE-RENDER
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.props.saveComment(this.state.comment);
+    this.setState({ comment: '' });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h4>Add a Comment</h4>
+        <textarea onChange={this.handleChange} value={this.state.comment} />
+        <div>
+          <button>Submit Content</button>
+        </div>
+      </form>
+    );
+  }
+};
+
+export default connect(null, actions)(CommentBox);
---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import CommentBox from 'components/CommentBox';
+import Root from 'Root';
+
+let component;
+
+beforeEach(() => {
+  component =  mount(
+    <Root>
+      <CommentBox />
+    </Root>
+  )
+});
+
+afterEach(() => {
+  component.unmount();
+});
+
+it('has a text area and a button', () => {
+  expect(component.find("textarea").length).toEqual(1);
+  expect(component.find("button").length).toEqual(1);
+});
+
+describe('the text area', () => {
+  beforeEach(() => {
+    component.find("textarea").simulate('change', {
+      target: { value: 'new comment' }
+    });
+    component.update();
+  })
+
+  it('has a text area that users can type in', () => {
+    expect(component.find("textarea").prop('value')).toEqual('new comment');
+  });
+  
+  it('when form is submitted, text area gets emptied', () => {
+    component.find("button").simulate('submit');
+    component.update();
+    expect(component.find("textarea").prop('value')).toEqual('');
+  });
+});

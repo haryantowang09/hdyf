ac5f0fdb5e11fb9f698092b6b6c8bb443d07dbb4
hwang09 <haryantowang09@gmail.com>
2020-05-09 15:26:10 +0800

    Add : service worker become PWA ready

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Root from 'Root';
 import App from 'components/App';
 import 'assets/styles/reset.css';
 import 'assets/styles/layout.css';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Root>
@@ -14,3 +15,8 @@ ReactDOM.render(
   </Root>,
   document.querySelector('#root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
